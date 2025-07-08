b25f2143e493c33abdd0670ad06ec2993cb2b749
Your Name <calumengee@gmail.com>
2025-07-08 21:19:20 +0700

    fix: wrap BlogList in Suspense to resolve useSearchParams prerender error on /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,13 @@
 import { getAllPosts } from "@/lib/blog";
 import BlogSearch from "@/components/shared/BlogList";
+import { Suspense } from "react";
 
 export default function BlogPage() {
   const posts = getAllPosts();
 
-  return <BlogSearch posts={posts} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogSearch posts={posts} />
+    </Suspense>
+  );
 }

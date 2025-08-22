d14e9fa5eae06a3cecf41bb2d497818e0d4f3432
annguyen1909 <garenalord1234@gmail.com>
2025-08-22 15:10:13 +0700

    update UET tag

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -40,6 +40,28 @@ export default function GoogleAnalytics() {
           gtag('config', '${GOOGLE_ADS_ID}');
         `}
       </Script>
+
+      {/* Microsoft Ads UET */}
+      <Script id="microsoft-ads-uet" strategy="afterInteractive">
+        {`
+          (function(w,d,t,r,u)
+          {
+              var f,n,i;
+              w[u]=w[u]||[],f=function()
+              {
+                  var o={ti:"187209135", enableAutoSpaTracking: true};
+                  o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")
+              },
+              n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function()
+              {
+                  var s=this.readyState;
+                  s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)
+              },
+              i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)
+          })
+          (window,document,"script","//bat.bing.com/bat.js","uetq");
+        `}
+      </Script>
     </>
   );
 } 
